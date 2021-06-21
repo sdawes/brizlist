8434c0e0d0f30d4c8b74b7efbc6770e98f4070ab
Stephen Dawes <sdawes@outlook.com>
2021-06-21 19:34:13 +0100

    add title and head to index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,19 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>brizlist.com</h1>
-      <p>This site has been built using Next.js and deployed using AWS Amplify and domains with Route 53</p>
-      <p>To come: dynamoDB and lambdas</p>
+      <Head>
+        <title>brizlist.com</title>
+        <link rel="icon" href="/favicon.io" />
+      </Head>
+      
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          brizlist.com
+        </h1>
+        <p>This site has been built using Next.js and deployed using AWS Amplify and domains with Route 53</p>
+        <p>To come: dynamoDB and lambdas</p>
+      </main>
+      
     </div>
   )
 }
