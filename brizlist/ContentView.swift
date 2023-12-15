b11c0c143e40195e6196ab050229217ae4450faa
Stephen Dawes <stephenrdawes@gmail.com>
2023-12-15 19:50:56 +0000

    Initial Commit

---
+++ brizlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  brizlist
+//
+//  Created by Stephen Dawes on 15/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ brizlist/brizlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  brizlistApp.swift
+//  brizlist
+//
+//  Created by Stephen Dawes on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct brizlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
