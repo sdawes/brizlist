4b71a9a3d50f4691fcbdb8564d1a72baeabe30c2
Stephen Dawes <stephenrdawes@gmail.com>
2023-12-15 20:01:21 +0000

    Updated text on Content view and removed image

--- brizlist/ContentView.swift
+++ brizlist/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Steve!")
+            Text("Hello, Steve! How are you?")
         }
         .padding()
     }
