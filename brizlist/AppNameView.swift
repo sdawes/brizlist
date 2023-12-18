85a19287ea0aa28f7c276e9f88b36f7a88e6f9fd
Stephen Dawes <stephenrdawes@gmail.com>
2023-12-18 16:39:48 +0000

    Added new APpName view for the title of the app

---
+++ brizlist/AppNameView.swift
@@ -0,0 +1,24 @@
+//
+//  AppNameView.swift
+//  brizlist
+//
+//  Created by Stephen Dawes on 18/12/2023.
+//
+
+import SwiftUI
+
+struct AppNameView: View {
+    var body: some View {
+        Text("brizlist")
+            .font(.largeTitle) // Customize the font as needed
+            .padding() // Add padding for better layout
+            // Add any other styling you want for the app name
+    }
+}
+
+
+#Preview {
+    AppNameView()
+}
+
+
