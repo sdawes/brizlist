0a084c868d074ca180a6fda7479b61ff1092e2a9
Stephen Dawes <stephenrdawes@gmail.com>
2023-12-18 09:08:54 +0000

    Rename venueview to venuelist view, changed mockdata and added venueof the week/ added new fields to multiple files

---
+++ brizlist/VenueOfTheWeekView.swift
@@ -0,0 +1,18 @@
+//
+//  VenueOfTheWeek.swift
+//  brizlist
+//
+//  Created by Stephen Dawes on 16/12/2023.
+//
+
+import SwiftUI
+
+struct VenueOfTheWeek: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VenueOfTheWeek()
+}
