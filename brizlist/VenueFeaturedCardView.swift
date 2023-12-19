a2c83809a2df7c0946893512bb3eed2fa3d190cc
Stephen Dawes <stephenrdawes@gmail.com>
2023-12-19 10:19:20 +0000

    Added new venue featured card view to display a featured venue at the otp of the list view

---
+++ brizlist/VenueFeaturedCardView.swift
@@ -0,0 +1,18 @@
+//
+//  VenueFeaturedCardView.swift
+//  brizlist
+//
+//  Created by Stephen Dawes on 18/12/2023.
+//
+
+import SwiftUI
+
+struct VenueFeaturedCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VenueFeaturedCardView()
+}
