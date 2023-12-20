02618afb22f61628240a33a6b451b20f051e036b
Stephen Dawes <stephenrdawes@gmail.com>
2023-12-20 09:41:09 +0000

    Added navigation stack to the venue view, and navigation links to a more detailed set of information for each venue

---
+++ brizlist/VenueDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  VenueDetailView.swift
+//  brizlist
+//
+//  Created by Stephen Dawes on 20/12/2023.
+//
+
+import SwiftUI
+
+struct VenueDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VenueDetailView()
+}
